f3c7711660d5599e7fda681040eddcda46c505dd
om333 <om123@gmail.com>
2024-12-30 10:24:21 +0530

    om commit

---
+++ om.sh
@@ -0,0 +1,5 @@
+#!/bin/bash   # it is an interpreter that execute  commands and syntax inside shellscript
+echo "hara hara mahadeva shambho shankara"
+echo "paravatahipathayaenamaha"
+echo "lord shiva(inside lord vishnu, lord brahma)"
+echo "lord shakthi ("paravatahi" , "lakshmi", "saraswathidevi")"
