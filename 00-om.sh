c388a063bfc44ff9fe8562d26bf7980568537bac
om <om@gmail.com>
2024-12-31 11:47:07 +0530

    rename the script

---
+++ 00-om.sh
@@ -0,0 +1,6 @@
+#!/bin/bash   # it is an interpreter that execute commands and syntax inside shellscript
+# shebang is compiler
+echo "hara hara mahadeva shambho shankara"
+echo "paravatahipathayaenamaha"
+echo "lord shiva(inside lord vishnu, lord brahma)"
+echo "lord shakthi ("paravatahidevi" , "lakshmi", "saraswathidevi")"
