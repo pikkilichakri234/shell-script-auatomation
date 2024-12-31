43d59a7e4be119b1c78abb5383f7806d41bb58d0
om <om@gmail.com>
2024-12-31 11:25:50 +0530

    create installation-script

---
+++ 08-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ USERID -ne 0 ]
+then
+  echo "please run this script with root access"
+  exit 1 # manual exit if error comes
+else
+  echo "you are superuser"
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+  echo "installation of mysql... failure"
+  exit 1
+else
+  echo "installation of mysql...success"
+fi
+
+dnf install maven -y
+if [ $? -ne 0 ]
+then
+  echo "installation of git... failure"
+  exit 1
+else
+  echo "installation of git... success"
