bd09586f6ac915a4214bdbe47921f015e1690560
om <om@gmail.com>
2024-12-31 10:57:11 +0530

    update & create array

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# array it is list of elements
+MOVIES=("omnamahashivaya","shivakesava","trimurthulu")
+ #index starts with 0, 1,2,..... 
+echo "first movies is: ${MOVIES[0]}"
+echo "second movies is: ${MOVIES[1]}"
+echo "third movies is: ${MOVIES[2]}"
+echo "all elemenets is: ${MOVIES[@]}"
+
