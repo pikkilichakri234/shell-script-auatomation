9e34a0eec11c28a32788b3aff952a93f821e421f
om <om@gmail.com>
2025-01-02 10:27:01 +0530

    color codes setup script

--- 11-color-code.sh
+++ 11-color-code.sh
@@ -5,12 +5,73 @@
  Y="\e[33m"
  N="\e[om"
 
- VALIDATE() {
-    if [ $1 -ne 0 ]
+ USERID=$(id -u)
+check_root() {
+ if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi   
+}
+VALIDATE() {
+    if [ $1 -ne 0 ] 
     then
-      echo  -e "$2 is ... $R failure $N"  
-      exit 1  # other then 0
-    else
-       echo -e "$2 is ... $G sucess $N"
+      echo "$2 is ... $R failed  $N"
+      exit 1
+    else 
+      echo "$2 is ... $G success $N "
     fi
- } 
+}
+check_root
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "install mysql"
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "installing git"
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
+
