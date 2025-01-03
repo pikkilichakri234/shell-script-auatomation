0642bd7502d955cb6eb17e08436343e2adff2479
om <om@gmail.com>
2025-01-03 10:36:49 +0530

    cron updated

---
+++ 16-cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# cron :- to schedule the works in intervals ( synatx:- * * * * *)
+# * * * * * = miniutes hours date month  day
+LOG_FILE=/tmp/om.log
+
+echo "hello cron log message is $(date)" >> "$LOG_FILE"
