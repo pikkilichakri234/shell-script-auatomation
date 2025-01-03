a5115705765bb2752b7680bde24f28acb0e0316e
om <om@gmail.com>
2025-01-03 11:24:19 +0530

    update shellscriptproject

--- expense-project-shellscript/frontend.sh
+++ expense-project-shellscript/frontend.sh
@@ -10,6 +10,7 @@ LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]
