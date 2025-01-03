623972547f28e09343784711af9efdf0d90dcd05
om <om@gmail.com>
2025-01-03 11:22:22 +0530

    update shellscriptproject

--- expense-project-shellscript/mysql.sh
+++ expense-project-shellscript/mysql.sh
@@ -8,7 +8,7 @@ LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-mkdir -p expense-logs
+mkdir -p $LOGS_FOLDER
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
