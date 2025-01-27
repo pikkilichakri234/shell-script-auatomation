830ec2f86db39839374facdfb3e288078daf3b7a
om333 <om123@gmail.com>
2025-01-27 20:40:44 +0530

    om

--- 04-special-vars.sh
+++ 04-special-vars.sh
@@ -17,4 +17,6 @@ echo "Generates a ranadom number: $RANDOM"
 echo "Turns on debugging for visibility: $SET -x"   # -X is enable debug , +X is disable debug
 echo "To measure execution duration: $SECONDS"
 echo "exit status of prevous commands is sucess or fail check: $?"
-
+echo " last argument of previous command is :: $_"
+echo "current option set for shell is :: $-"
+echo " to store the history commands is :: $HISTFILE" # HISTFIE store commands by default 1k" , shell file limit default 255 characters stored
