c39b95500e4b5ee98c18e12ef7ce097714056783
om333 <om123@gmail.com>
2025-01-27 18:39:12 +0530

    om

--- 35-author.sh
+++ 35-author.sh
@@ -2,4 +2,4 @@
 Author: omnamahashivaya
 Date: 27-Jan-2025
 Purpose: Display author information and a simple greeting
-echo "update shell scripting in some tasks"s
+echo "update shell scripting in some tasks"
